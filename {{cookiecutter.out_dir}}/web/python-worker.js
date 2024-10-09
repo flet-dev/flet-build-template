5b00ffb4399284cdf9c0bb601a097a973aadeb1c
Feodor Fitsner <feodor@appveyor.com>
2024-10-09 12:18:32 -0700

    For Flet 0.25.0 (#20)
    * path to serious_python
    
    * full path to serious_python
    
    * doNotStrip
    
    * change abi filters order
    
    * change order again
    
    * split_per_abi
    
    * make an error in template
    
    * Fix template
    
    * Test info_plist
    
    * info plist complete
    
    * Fix elif
    
    * Another fix
    
    * Another try
    
    * simplify if
    
    * simple test
    
    * no v
    
    * remove everything
    
    * remove other options
    
    * restore infoplist
    
    * equal not is
    
    * one line
    
    * add macos_entitlements
    
    * empty list
    
    * empty array
    
    * macos entitlements
    
    * No hard-coded entitlements
    
    * Android manifest
    
    * Fix manifest
    
    * Fix template again
    
    * Remove hard-coded permissions
    
    * Fix template
    
    * true and false
    
    * remove hard-coded permissions
    
    * Android deep linking
    
    Close #4025
    
    * Template cleanup
    
    * Signing Android builds
    
    * Fix signing options
    
    * Set FLET_APP_DATA and FLET_APP_TEMP dirs for the app
    
    * Pyodide 0.26.2
    
    * serious_python: 0.8.0

--- {{cookiecutter.out_dir}}/web/python-worker.js
+++ {{cookiecutter.out_dir}}/web/python-worker.js
@@ -1,4 +1,4 @@
-importScripts("https://cdn.jsdelivr.net/pyodide/v0.25.0/full/pyodide.js");
+importScripts("https://cdn.jsdelivr.net/pyodide/v0.26.2/full/pyodide.js");
 
 self.pythonModuleName = null;
 self.initialized = false;
