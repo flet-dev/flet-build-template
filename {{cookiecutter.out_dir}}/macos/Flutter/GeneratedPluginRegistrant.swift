a6e46c8db125c8d80cb9f8cfbf65166bacf2ff03
Feodor Fitsner <feodor@appveyor.com>
2025-01-15 11:18:29 -0800

    Added support for developer mode

--- {{cookiecutter.out_dir}}/macos/Flutter/GeneratedPluginRegistrant.swift
+++ {{cookiecutter.out_dir}}/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,8 @@ import Foundation
 
 import package_info_plus
 import path_provider_foundation
-import screen_retriever
+import record_darwin
+import screen_retriever_macos
 import serious_python_darwin
 import shared_preferences_foundation
 import url_launcher_macos
@@ -18,7 +19,8 @@ import window_to_front
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
+  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SeriousPythonPlugin.register(with: registry.registrar(forPlugin: "SeriousPythonPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
