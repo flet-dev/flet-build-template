3bf678a18115ccc4e94d59c87dd20bcf59a35796
Feodor Fitsner <feodor@appveyor.com>
2025-03-04 12:21:50 -0800

    Cleanup

--- cookiecutter_extensions.py
+++ cookiecutter_extensions.py
@@ -2,10 +2,9 @@ from jinja2 import pass_context
 from jinja2.ext import Extension
 
 
-class FoobarExtension(Extension):
+class FletExtension(Extension):
     def __init__(self, environment):
-        super(FoobarExtension, self).__init__(environment)
-        environment.filters["foobar"] = lambda v: v * 2
+        super(FletExtension, self).__init__(environment)
         environment.globals["get_pyproject"] = self.get_pyproject
 
     @pass_context
