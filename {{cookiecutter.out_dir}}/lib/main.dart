940e244d0b80c8d12713d014745061375ff93ad2
Feodor Fitsner <feodor@appveyor.com>
2025-12-03 18:44:39 -0800

    Update url_strategy import and add flutter_web_plugins
    Replaced the url_strategy import with flutter_web_plugins/url_strategy in main.dart and added flutter_web_plugins to pubspec.yaml dependencies to ensure compatibility and proper URL strategy support.

--- {{cookiecutter.out_dir}}/lib/main.dart
+++ {{cookiecutter.out_dir}}/lib/main.dart
@@ -9,7 +9,7 @@ import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart' as path_provider;
 import 'package:serious_python/serious_python.dart';
-import 'package:url_strategy/url_strategy.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:window_manager/window_manager.dart';
 
 import "python.dart";
