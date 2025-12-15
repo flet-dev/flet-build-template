1a0483ff0ef55eac980b9789e4a31cb49be3a2af
Feodor Fitsner <feodor@appveyor.com>
2025-12-15 14:09:33 -0800

    Revert "Add random suffix to UDS socket filename"
    This reverts commit bcff44e8e136cceaafa527de0bb95e06d51e9303.

--- {{cookiecutter.out_dir}}/lib/main.dart
+++ {{cookiecutter.out_dir}}/lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:io';
-import 'dart:math';
 
 import 'package:flet/flet.dart';
 import 'package:flutter/foundation.dart';
@@ -61,13 +60,6 @@ String pageUrl = "";
 String assetsDir = "";
 String appDir = "";
 Map<String, String> environmentVariables = {};
-final _random = Random();
-
-String _randomString(int length) {
-  const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
-  return String.fromCharCodes(Iterable.generate(
-      length, (_) => chars.codeUnitAt(_random.nextInt(chars.length))));
-}
 
 void main(List<String> args) async {
   _args = List<String>.from(args);
@@ -201,8 +193,7 @@ Future prepareApp() async {
       environmentVariables["FLET_SERVER_PORT"] = tcpPort.toString();
     } else {
       // use UDS on other platforms
-      var socketSuffix = _randomString(8);
-      pageUrl = "flet_${pid}_$socketSuffix.sock";
+      pageUrl = "flet_$pid.sock";
       environmentVariables["FLET_SERVER_UDS_PATH"] = pageUrl;
     }
   }
