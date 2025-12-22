84c512c0c6bc70e6564ba8ccc74784f0903cd0da
Feodor Fitsner <feodor@appveyor.com>
2025-12-22 12:37:47 -0800

    Update temp directory usage for socket paths
    Replaced getApplicationCacheDirectory with getTemporaryDirectory for app temp path. Updated socket file paths to use the correct temp directory, ensuring consistency and proper file placement across platforms.

--- {{cookiecutter.out_dir}}/lib/main.dart
+++ {{cookiecutter.out_dir}}/lib/main.dart
@@ -168,7 +168,7 @@ Future prepareApp() async {
     // configure apps DATA and TEMP directories
     WidgetsFlutterBinding.ensureInitialized();
 
-    var appTempPath = (await path_provider.getApplicationCacheDirectory()).path;
+    var appTempPath = (await path_provider.getTemporaryDirectory()).path;
     var appDataPath =
         (await path_provider.getApplicationDocumentsDirectory()).path;
 
@@ -198,7 +198,7 @@ Future prepareApp() async {
       environmentVariables["FLET_SERVER_PORT"] = tcpPort.toString();
     } else {
       // use UDS on other platforms
-      pageUrl = "flet_$pid.sock";
+      pageUrl = path.join(appTempPath, "flet_$pid.sock");
       environmentVariables["FLET_SERVER_UDS_PATH"] = pageUrl;
     }
   }
@@ -227,7 +227,7 @@ Future<String?> runPythonApp(List<String> args) async {
         'Python output TCP Server is listening on port ${outSocketServer.port}');
     socketAddr = "$tcpAddr:${outSocketServer.port}";
   } else {
-    socketAddr = "stdout_$pid.sock";
+    socketAddr = path.join(environmentVariables["FLET_APP_STORAGE_TEMP"]!, "stdout_$pid.sock");
     if (await File(socketAddr).exists()) {
       await File(socketAddr).delete();
     }
